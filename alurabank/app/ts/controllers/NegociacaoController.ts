b525e68e2cf6357e9139287d1f7c04b0def5d1dc
Paulo Henrique D'Avila de Oliveira Alonso <alopaulohenrique@gmail.com>
2021-03-24 16:41:44 -0300

    foi

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,23 +1,23 @@
 class NegociacaoController {
 
-    private _inputData: any;
-    private _inputQuantidade: any;
-    private _inputValor: any;
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
 
     constructor() {
-        this._inputData = document.querySelector('#data');
-        this._inputQuantidade = document.querySelector('#quantidade');
-        this._inputValor = document.querySelector('#valor');
+        this._inputData = <HTMLInputElement>document.querySelector('#data');
+        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
+        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
     }
 
-    adiciona(event) {
+    adiciona(event: Event) {
 
         event.preventDefault();
 
         const negociacao = new Negociacao(
-            this._inputData.value,
-            this._inputQuantidade.value,
-            this._inputValor.value);
+            new Date(this._inputData.value.replace(/-/g, ',')),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value));
 
         console.log(negociacao);
     }
